7c91d1a918623d7b536a2d8d4f44523b90a56ea8
Mohamed9555 <thegame9555@gmail.com>
2023-08-03 04:07:38 +0300

    Sourcing Issue
    Solved the sourcing issue now files able to not have execute premission while chaning user prompt as we need to using export in the main script

--- db_engine.sh
+++ db_engine.sh
@@ -17,24 +17,31 @@ echo -e "${RESET}You can choose from the following options:"
 
 
 options=("Create Database" "Drop Database" "List Databases" "Connect to Database" "Quit")
-PS3="Enter the number of option: "
+PS3="Enter the number of database operation: "
+original_ps3=$PS3
 
 select option in "${options[@]}" 
 do
     case "$option" in
         "Create Database")
-            # create_database.sh
-            source ../create_database.sh
+            export PS3="choose one of the following: "
+            bash ../create_database.sh
+            PS3=$original_ps3
             ;;
         "Drop Database")
-            echo "Hello"
-            # drop_database.sh
+            export PS3="Choose Database: "
+            bash ../drop_database.sh
+            PS3=$original_ps3
             ;;
         "List Databases")
-            # list_databases.sh
+            export PS3="Choose Database: "
+            bash ../list_databases.sh
+            PS3=$original_ps3
             ;;
         "Connect to Database") 
-            # connect_database.sh
+            export PS3="Choose Database: "
+            bash ../connect_to_database.sh
+            PS3=$original_ps3
             ;;
         "Quit")
             echo "Goodbye!" 
