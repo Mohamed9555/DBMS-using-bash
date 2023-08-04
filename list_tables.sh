8375a600e725b9533086ca78fee6489bda2cfba1
Mohamed9555 <thegame9555@gmail.com>
2023-08-04 23:48:39 +0300

    added intitial list_databases script

---
+++ list_tables.sh
@@ -0,0 +1,29 @@
+#!bin/bash
+
+
+
+tables=($(ls -F | grep -v /)) # not ending with /
+
+
+if [ ${#tables[@]} != 0 ]
+then
+    echo "Available tables: ${tables[@]} "  
+    read -p "Would you like to show certian table Y/N: " show
+    if [ $show = "y" ] || [ $show = "Y" ]
+    then
+    PS3="Choose table: "
+        select table in "${tables[@]%/}" 
+        do
+            if [ -n "$table" ] # check the number 
+            then
+                cat $table
+                # echo $PWD
+                break
+            else
+                echo "Invalid option"
+            fi
+        done
+    fi
+else
+    echo "No tables found"
+fi
