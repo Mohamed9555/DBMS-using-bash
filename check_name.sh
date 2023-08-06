1ff117280edb6a413a6d78a3525c9f63eed609c5
Mohamed9555 <thegame9555@gmail.com>
2023-08-06 16:03:25 +0300

    check_table_name

--- check_name.sh
+++ check_name.sh
@@ -4,7 +4,7 @@
 check_database_name() {
     local db_name="$1"
     local flag=1
-    local max_length=30
+    local max_length=64
 
 
     if [[ $db_name =~ [^a-zA-Z0-9_] ]];
@@ -34,4 +34,71 @@ check_database_name() {
     return 1
 }
 
+
+check_table_name() {
+    local table_name="$1"
+    local flag=1
+    local max_length=64
+
+
+    if [[ $table_name =~ [^a-zA-Z0-9_] ]];
+    then
+        echo "Invalid Naming, table name can't contain special characters, Please try again "
+        return 0
+    fi
+
+    if [[ "$table_name" =~ ^[^a-zA-Z].* ]];
+    then
+        echo "Invalid Naming, table name should Start with a character, Please try again "
+        return 0
+    fi
+
+    if [[ -z "$table_name" ]];
+    then
+        echo "Input can't be empty, Please try again"
+        return 0
+    fi
+
+    if [[ ${table_name} -gt $max_length ]];
+    then
+        echo "Invalid Naming, table name exceeded the maximum length limit of $max_length characters. Try again "
+        return 0
+    fi
+
+    return 1
+}
+
+
+check_column_name() {
+    local table_name="$1"
+    local flag=1
+    local max_length=64
+
+
+    if [[ $table_name =~ [^a-zA-Z0-9_] ]];
+    then
+        echo "Invalid Naming, table name can't contain special characters, Please try again "
+        return 0
+    fi
+
+    if [[ "$table_name" =~ ^[^a-zA-Z].* ]];
+    then
+        echo "Invalid Naming, table name should Start with a character, Please try again "
+        return 0
+    fi
+
+    if [[ -z "$table_name" ]];
+    then
+        echo "Input can't be empty, Please try again"
+        return 0
+    fi
+
+    if [[ ${table_name} -gt $max_length ]];
+    then
+        echo "Invalid Naming, table name exceeded the maximum length limit of $max_length characters. Try again "
+        return 0
+    fi
+
+    return 1
+}
 
